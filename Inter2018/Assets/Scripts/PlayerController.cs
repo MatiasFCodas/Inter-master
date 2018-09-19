6b5b61c1a16ac64cfc5f75149d907c90a1367474
Saitam000 <matiascodas@gmail.com>
2018-09-19 13:19:23 -0300

    Initial commit

---
+++ Inter2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float moveSpeed = 20.0f;
+	private Vector3 direction;
+	private CharacterController characterController;
+
+	public Camera cam;
+
+
+	private float distance = 30f;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		direction.x = Input.GetAxis ("Horizontal");
+		direction.z = Input.GetAxis ("Vertical");
+
+		Vector3 moveDirection = new Vector3 (direction.x, 0, direction.z);
+		characterController.SimpleMove (moveDirection * moveSpeed);
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Inter
+Inter do 5º semestre, vespertino, 2018
+
+
+Galera, esse é o repositório do nosso inter - By Luken
+
+Teste K
+
